b165c007ecee77c1542d22daea30c3061fda62fe
Pablo <123060435+PabloCuberoLuque@users.noreply.github.com>
2023-10-04 13:29:43 +0200

    Ejercicio ComboBox añadido

---
+++ Ejercicio/Persona.java
@@ -0,0 +1,18 @@
+package segundodam.Interfaces.Ejercicio;
+
+public class Persona {
+    
+    String nombre;
+    int edad;
+    String profesion;
+
+    public Persona(String nombre, int edad, String profesion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.profesion = profesion;
+    }
+    
+    
+
+    
+}
